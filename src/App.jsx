2d7763e1cea7a2926e8bdd5c1af7c2733c6d806e
BNMAMA <ksuyeon56@naver.com>
2025-08-08 21:58:06 +0900

    Feat: state

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-
-import './App.css'
-import BasicJsx from './components/Basicjsx/BasicJsx';
-import BasicProps from './components/BasicProps/BasicProps';
-import BasicReact from './components/BasicReact/BasicReact'
-import CountState from './components/BasicState/CountState/CountState';
-
-
+import "./App.css";
+import BasicJsx from "./components/BasicJsx/BasicJsx";
+import BasicProps from "./components/BasicProps/BasicProps";
+import BasicReact from "./components/BasicReact/BasicReact";
+import Calculator from "./components/BasicState/Calculator/Calculator";
+import CountState from "./components/BasicState/CountState/CountState";
 
 function App() {
-   return (
-    <>
-    {/* <BasicReact /> */}
-    {/* <BasicJsx /> */}
-    {/* <BasicProps /> */}
-    <CountState />
-  </>
-   );
-  
+	return (
+		<>
+			{/* <BasicReact /> */}
+			{/* <BasicJsx /> */}
+			{/* <BasicProps /> */}
+			<CountState />
+			{/* <Calculator /> */}
+		</>
+	);
 }
 
 export default App;
